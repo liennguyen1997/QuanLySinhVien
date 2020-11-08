ace43c69736bb8965c6d0bd7d693a85cdd6d6fda
liên nguyễn <leniliennguyen@gmail.com>
2020-11-08 16:18:43 +0700

    them Monhoc

---
+++ modals/Monhoc.js
@@ -0,0 +1,8 @@
+var Monhoc = function () {
+    this.maMonHoc = '';
+    this.tenMonHoc = '';
+    this.xuatThongTinMonHoc = function () {
+        console.log('ma mon hoc: ${this.maMonHoc}');
+        console.log('Ten Mon Hoc: ${this.tenMonHoc}')
+    }
+}
